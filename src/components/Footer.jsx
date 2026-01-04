119bb62e2a1bf71532f4fec26a8af6afb1ee3947
zizozizo123km <nnacar12@gmail.com>
2026-01-04 11:58:58 +0100

    Update Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2026 not. جميع الحقوق محفوظة.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
