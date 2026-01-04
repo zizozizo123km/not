358bac8a83458f64b2029978bd23c8bedee94573
zizozizo123km <nnacar12@gmail.com>
2026-01-04 11:58:51 +0100

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AuthProvider } from './context/AuthContext';
+import AppRouter from './router/AppRouter';
+
+function App() {
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  );
+}
+
+export default App;
