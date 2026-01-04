e7046ced54085b853f6aecad3b3227ab4df96de5
zizozizo123km <nnacar12@gmail.com>
2026-01-04 11:59:19 +0100

    Update AppRouter.jsx

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <div className="app-layout">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
